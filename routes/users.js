ebe1e1a7116708c8cbdb9f57a1d6fb57caba6217
Gaurav Seth <post2seth@gmail.com>
2019-07-06 00:13:05 +0530

    minor cleanup

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users');
+    res.render('users');
 });
 
 module.exports = router;
